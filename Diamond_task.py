4575799b1905514ec6a6d2575d6a4114e214459b
Rajvi Zala <89260078+rajvizala@users.noreply.github.com>
2022-03-02 17:53:27 +0530

    Add files via upload

---
+++ Diamond_task.py
@@ -0,0 +1,20 @@
+rows=int(input("enter no of rows:"))
+k=rows-1
+for i in range(rows):
+    for s in range(k,0,-1):
+        print(end=" ")
+    k=k-1
+    for j in range(i+1):
+        print("*",end=" ")
+    print(" ")
+
+k=rows-(rows-1)
+for n in range(rows):
+    for r in range(k,0,-1):
+        print(end=" ")
+    k=k+1
+    for p in range(i):
+        print("*",end=" ")
+    print(" ")
+    i=i-1
+
