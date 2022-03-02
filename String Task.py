e7d7ad0f32621257c1f603ad34abaf040f9e5067
Dev Shinde <84491890+dev-shinde@users.noreply.github.com>
2022-03-02 09:43:21 +0530

    Add files via upload

---
+++ String Task.py
@@ -0,0 +1,13 @@
+# String Task
+
+# 1. Reverse string 
+
+# a="Hello Everybody" [::-1]
+# print (a)
+
+# 2. Word Wise Reverse
+
+str_ey = input ("Enter your text:- ")
+ls = str_ey.split(' ')
+ls(str_ey)
+print(str_ey[::-1])
